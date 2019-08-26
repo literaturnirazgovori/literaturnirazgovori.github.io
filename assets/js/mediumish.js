955117545f01fb22e89e2681a448913ab506f3b8
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2019-08-26 15:42:53 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -14,6 +14,29 @@ jQuery(document).ready(function($){
       }
     }
   });
+
+  //---- search bar ---------
+  //var facebookIframe = document.querySelector('#facebook_iframe');
+  setupFBframe();
+ 
+  //-------- Facebook banner -------
+    window.addEventListener("resize", resizeThrottler, false);
+    var resizeTimeout;
+
+    function resizeThrottler() {
+      if (!resizeTimeout) {
+        resizeTimeout = setTimeout(function() {
+          resizeTimeout = null;
+          actualResizeHandler();
+        }, 66);
+      }
+    }
+    function actualResizeHandler() {
+      setupFBframe();
+    }
+    //--------/Facebook banner -------
+
+
   $("#search-close").click(function (){
     hideSearch();
   });
@@ -31,72 +54,70 @@ jQuery(document).ready(function($){
       }
   });
 
-  
   //---- search bar ---------
-
-    var offset = 1250; 
-    var duration = 800; 
-    jQuery(window).scroll(function() { 
-        if (jQuery(this).scrollTop() > offset) { 
-        jQuery('.back-to-top').fadeIn(duration); 
-        } else { 
-        jQuery('.back-to-top').fadeOut(duration); 
-        }
-    });
-    jQuery('.back-to-top').click(function(event) { 
-    event.preventDefault(); 
-    jQuery('html, body').animate({scrollTop: 0}, duration); 
-    return false; 
-    })
-
-
-    // alertbar later
-    $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 280) {
-            $('.alertbar').fadeIn();
-        } else {
-            $('.alertbar').fadeOut();
-        }
-    });
+  var offset = 1250; 
+  var duration = 800; 
+  jQuery(window).scroll(function() { 
+      if (jQuery(this).scrollTop() > offset) { 
+      jQuery('.back-to-top').fadeIn(duration); 
+      } else { 
+      jQuery('.back-to-top').fadeOut(duration); 
+      }
+  });
+  jQuery('.back-to-top').click(function(event) { 
+  event.preventDefault(); 
+  jQuery('html, body').animate({scrollTop: 0}, duration); 
+  return false; 
+  })
 
 
-        // Smooth scroll to an anchor
-        $('a.smoothscroll[href*="#"]')
-          // Remove links that don't actually link to anything
-          .not('[href="#"]')
-          .not('[href="#0"]')
-          .click(function(event) {
-            // On-page links
-            if (
-              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-              &&
-              location.hostname == this.hostname
-            ) {
-              // Figure out element to scroll to
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-              // Does a scroll target exist?
-              if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
-                  scrollTop: target.offset().top
-                }, 1000, function() {
-                  // Callback after animation
-                  // Must change focus!
-                  var $target = $(target);
-                  $target.focus();
-                  if ($target.is(":focus")) { // Checking if the target was focused
-                    return false;
-                  } else {
-                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
-                  };
-                });
-              }
-            }
+  // alertbar later
+  $(document).scroll(function () {
+      var y = $(this).scrollTop();
+      if (y > 280) {
+          $('.alertbar').fadeIn();
+      } else {
+          $('.alertbar').fadeOut();
+      }
+  });
+
+
+  // Smooth scroll to an anchor
+  $('a.smoothscroll[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
           });
+        }
+      }
+    });
     
     
     // Hide Header on on scroll down
@@ -141,8 +162,7 @@ jQuery(document).ready(function($){
 
         lastScrollTop = st;
     }
-    
-    
+
     $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
 });
 
@@ -160,3 +180,35 @@ function showSearch()
   $("#search-wrapper").css('display', 'flex');
   $('#search-wrapper').animate({ height: 60}, 400, function() { $("#search-text").focus(); });
 }
+
+//-------- Facebook banner -------
+function setupFBframe() {
+  var frame  = document.querySelector('#facebook_iframe');
+  var container = frame.parentNode;
+
+  var containerWidth = container.offsetWidth;
+  var containerHeight = document.querySelector (".sitetitlecontainer").scrollHeight - 10;
+
+  if((!frame.src) || (frame.height != containerHeight)|| (frame.width != containerWidth))
+  {
+    frame.removeAttribute('src');
+    var src =
+      "https://www.facebook.com/plugins/page.php" +
+      "?href=https%3A%2F%2Fwww.facebook.com%2Fliteraturnirazgovori" +
+      "&width=" +
+      containerWidth +
+      "&height=" +
+      containerHeight +
+      "&small_header=false" +
+      "&adapt_container_width=false" +
+      "&hide_cover=false" +
+      "&hide_cta=true" +
+      "&show_facepile=false" +
+      "&appId";
+
+    frame.width = containerWidth;
+    frame.height = containerHeight;
+    frame.src = src;
+  }
+}
+//--------/Facebook banner -------
