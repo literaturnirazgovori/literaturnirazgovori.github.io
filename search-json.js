44c3cfb23a131cfa8bb06c4dbd4c72c5a96e67f8
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2020-09-02 10:17:43 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Против природата&quot; на Томас Еспедал - щастието е противоестествено, болката не",
+            "url": "/bookreviews/2020/09/02/10-16-%D0%BF%D1%80%D0%BE%D1%82%D0%B8%D0%B2-%D0%BF%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D1%82%D0%BE%D0%BC%D0%B0%D1%81-%D0%B5%D1%81%D0%BF%D0%B5%D0%B4%D0%B0%D0%BB-%D1%89%D0%B0%D1%81%D1%82%D0%B8%D0%B5%D1%82%D0%BE-%D0%B5-%D0%BF%D1%80%D0%BE%D1%82%D0%B8%D0%B2%D0%BE%D0%B5%D1%81%D1%82%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BE-%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B5.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Томас Еспедал, Срещу природата, Персей, роман, рецензия",
+            "date": "2020-09-02 00:00:00 +0000",
+            "image": "/Uploads/sreshtuprirodata.jpg",
+            "author": "Antonia",
+            "subtitle": "Темата &quot;против природата&quot; е основна в книгата. Но тя съвсем не е еднозначна и затова лично аз разчитам заглавието провокативно, с обърнато значение"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Романът на Евгений Водолазкин &quot;Авиатор&quot; вече на български",
             "url": "/newbooks/2020/09/01/15-47-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD%D1%8A%D1%82-%D0%BD%D0%B0-%D0%B5%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2%D0%BE%D0%B4%D0%BE%D0%BB%D0%B0%D0%B7%D0%BA%D0%B8%D0%BD-%D0%B0%D0%B2%D0%B8%D0%B0%D1%82%D0%BE%D1%80-%D0%B2%D0%B5%D1%87%D0%B5-%D0%BD%D0%B0-%D0%B1%D1%8A%D0%BB%D0%B3%D0%B0%D1%80%D1%81%D0%BA%D0%B8.html",
             "category": "newbooks",
