7b42748d8a490243dde08efc5379a70220ebb902
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2020-11-06 08:58:45 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Любов на Boogie Street&quot; повдига въпроса за сексуалното привличане между баща и дъщеря",
+            "url": "/bookreviews/2020/11/06/10-56-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2-%D0%BD%D0%B0-boogie-street-%D0%B7%D0%B0-%D1%82%D1%8A%D1%80%D1%81%D0%B5%D0%BD%D0%B5%D1%82%D0%BE-%D0%BD%D0%B0-%D1%81%D0%B5%D0%BA%D1%81%D1%83%D0%B0%D0%BB%D0%BD%D0%B0%D1%82%D0%B0-%D0%B8%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%87%D0%BD%D0%BE%D1%81%D1%82-%D0%B2-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2%D1%82%D0%B0.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Любов на Boogie Street, Златко Ангелов, Скалино, роман, рецензия",
+            "date": "2020-11-06 00:00:00 +0000",
+            "image": "/Uploads/lyubov-na-boogie-street-31.jpg",
+            "author": "Antonia",
+            "subtitle": "Чрез техните изповеди в първо лице Златко Ангелов изследва две бисексуални жени и двама алфа-мъжкари, които търсят сексуалната си идентичност в контекста на любовта"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;Винилови души&quot; на Цветозар Цаков - пространство за възможни светове и комична абсурдност",
             "url": "/bookreviews/2020/11/04/11-19-%D0%B2%D0%B8%D0%BD%D0%B8%D0%BB%D0%BE%D0%B2%D0%B8-%D0%B4%D1%83%D1%88%D0%B8-%D0%BD%D0%B0-%D1%86%D0%B2%D0%B5%D1%82%D0%BE%D0%B7%D0%B0%D1%80-%D1%86%D0%B0%D0%BA%D0%BE%D0%B2-%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%BE-%D0%B7%D0%B0-%D1%84%D0%B0%D0%BD%D1%82%D0%B0%D0%B7%D0%B8%D0%B8-%D0%B8-%D0%BA%D0%BE%D0%BC%D0%B8%D1%87%D0%BD%D0%B0-%D0%B0%D0%B1%D1%81%D1%83%D1%80%D0%B4%D0%BD%D0%BE%D1%81%D1%82.html",
             "category": "bookreviews",
@@ -75,22 +91,6 @@ var search_json_index = [
             "image": "/Uploads/novoergo.jpg",
             "author": "Antonia",
             "subtitle": "Те са забавната и мистериозна „Събуждане в Шибуя“ от чешката дебютантка Анна Цима и изпълнената с черен хумор „Моят танк“ от унгареца Виктор Хорват"
-        } 
-        ,
-        
-    
-  
-          
-        {
-            "title": "&quot;Любов на Boogie Street&quot; повдига въпроса за сексуалното привличане между баща и дъщеря",
-            "url": "/bookreviews/2020/10/27/12-15-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2-%D0%BD%D0%B0-boogie-street-%D0%B7%D0%B0-%D1%82%D1%8A%D1%80%D1%81%D0%B5%D0%BD%D0%B5%D1%82%D0%BE-%D0%BD%D0%B0-%D1%81%D0%B5%D0%BA%D1%81%D1%83%D0%B0%D0%BB%D0%BD%D0%B0%D1%82%D0%B0-%D0%B8%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%87%D0%BD%D0%BE%D1%81%D1%82-%D0%B2-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2%D1%82%D0%B0.html",
-            "category": "bookreviews",
-            "category2": "",
-            "tags": "Любов на Boogie Street, Златко Ангелов, Скалино, роман, рецензия",
-            "date": "2020-10-27 00:00:00 +0000",
-            "image": "/Uploads/lyubov-na-boogie-street-31.jpg",
-            "author": "Antonia",
-            "subtitle": "Чрез техните изповеди в първо лице Златко Ангелов изследва две бисексуални жени и двама алфа-мъжкари, които търсят сексуалната си идентичност в контекста на любовта"
         } 
         ,
         
