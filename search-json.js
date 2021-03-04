a5206937ce84a2b77e77ad2c17dc94b9e521e9b8
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-03-04 11:31:18 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Всяка история е своеобразна манипулация. Разговор с Горан Войнович",
+            "url": "/interviews/2021/03/04/11-29-%D0%B2%D1%81%D1%8F%D0%BA%D0%B0-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%B5-%D1%81%D0%B2%D0%BE%D0%B5%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BD%D0%B0-%D0%BC%D0%B0%D0%BD%D0%B8%D0%BF%D1%83%D0%BB%D0%B0%D1%86%D0%B8%D1%8F-%D1%80%D0%B0%D0%B7%D0%B3%D0%BE%D0%B2%D0%BE%D1%80-%D1%81-%D0%B3%D0%BE%D1%80%D0%B0%D0%BD-%D0%B2%D0%BE%D0%B9%D0%BD%D0%BE%D0%B2%D0%B8%D1%87.html",
+            "category": "interviews",
+            "category2": "",
+            "tags": "Горан Войнович, интервю, Смокинята, Югославия",
+            "date": "2021-03-04 00:00:00 +0000",
+            "image": "/Uploads/goran-vojnovic.jpg",
+            "author": "Antonia",
+            "subtitle": "Литературата е опит за разбиране на човека и най-хубавото доказателство, че разбирането не е нещо просто, а ние дори и вече не се стараем да сме разбираеми"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Излезе &quot;Моята година със Селинджър&quot; – поглед към личността на легендарния писател",
             "url": "/newbooks/2021/03/02/10-04-%D0%B8%D0%B7%D0%BB%D0%B5%D0%B7%D0%B5-%D0%BC%D0%BE%D1%8F%D1%82%D0%B0-%D0%B3%D0%BE%D0%B4%D0%B8%D0%BD%D0%B0-%D1%81%D1%8A%D1%81-%D1%81%D0%B5%D0%BB%D0%B8%D0%BD%D0%B4%D0%B6%D1%8A%D1%80-%D0%BF%D0%BE%D0%B3%D0%BB%D0%B5%D0%B4-%D0%BA%D1%8A%D0%BC-%D0%BB%D0%B8%D1%87%D0%BD%D0%BE%D1%81%D1%82%D1%82%D0%B0-%D0%BD%D0%B0-%D0%BB%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D0%BD%D0%B8%D1%8F-%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D0%B5%D0%BB.html",
             "category": "newbooks",
