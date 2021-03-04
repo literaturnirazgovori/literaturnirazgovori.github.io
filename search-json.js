eefcd3290dfe211d570faeedb440136d17c2311e
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-03-04 12:56:24 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,7 +2,7 @@ var search_json_index = [
     
           
         {
-            "title": "Всяка история е своеобразна манипулация. Разговор с Горан Войнович",
+            "title": "Разговор с Горан Войнович: Всяка история е своеобразна манипулация",
             "url": "/interviews/2021/03/04/11-29-%D0%B2%D1%81%D1%8F%D0%BA%D0%B0-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%B5-%D1%81%D0%B2%D0%BE%D0%B5%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D0%BD%D0%B0-%D0%BC%D0%B0%D0%BD%D0%B8%D0%BF%D1%83%D0%BB%D0%B0%D1%86%D0%B8%D1%8F-%D1%80%D0%B0%D0%B7%D0%B3%D0%BE%D0%B2%D0%BE%D1%80-%D1%81-%D0%B3%D0%BE%D1%80%D0%B0%D0%BD-%D0%B2%D0%BE%D0%B9%D0%BD%D0%BE%D0%B2%D0%B8%D1%87.html",
             "category": "interviews",
             "category2": "",
