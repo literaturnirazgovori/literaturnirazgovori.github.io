3e9fdeb231d89fcb1dbb0069ca51f3301cfc1a14
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-04-18 10:13:37 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -1,6 +1,20 @@
 var search_json_index = [
     
-      
+          
+        {
+            "title": "Колонката на Георги Цанков: Айн Ранд, Кундера, Януш Корчак, Кирил Христов и още",
+            "url": "/kolonkata-na-georgi-cankov/2021/04/18/09-26-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D0%B0%D0%B9%D0%BD-%D1%80%D0%B0%D0%BD%D0%B4-%D0%BA%D1%83%D0%BD%D0%B4%D0%B5%D1%80%D0%B0-%D1%8F%D0%BD%D1%83%D1%88-%D0%BA%D0%BE%D1%80%D1%87%D0%B0%D0%BA-%D0%BA%D0%B8%D1%80%D0%B8%D0%BB-%D1%85%D1%80%D0%B8%D1%81%D1%82%D0%BE%D0%B2-%D0%B8-%D0%BE%D1%89%D0%B5.html",
+            "category": "kolonkata-na-georgi-cankov",
+            "category2": "kolonkata-na-georgi-cankov",
+            "tags": "Айн Ранд, Януш Корчак, Пресън Чайлд, Ленард Пийкоф, Румяна Халачева, Милан Кундера, Кирил Христов, Валерия Луизели",
+            "date": "2021-04-18 00:00:00 +0000",
+            "image": "/Uploads/kgt18april.jpg",
+            "author": "Guest",
+            "subtitle": "Да си сложиш таралеж в гащите – точно това е да обещаеш да пишеш за Айн Ранд и за новопоявилата се книга на Ленард Пийкоф &quot;Обективизмът: философията на Айн Ранд&quot;"
+        } 
+        ,
+        
+    
   
           
         {
