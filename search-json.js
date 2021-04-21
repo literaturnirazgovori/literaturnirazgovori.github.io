b122ae5e170b6d1a1848908ac992707219d7a091
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-04-21 10:46:19 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Излезе &quot;Момчетата от &quot;Никел&quot; - роман, който донесе втори &quot;Пулицър&quot; на Колсън Уайтхед",
+            "url": "/newbooks/2021/04/21/10-44-%D0%B8%D0%B7%D0%BB%D0%B5%D0%B7%D0%B5-%D0%BC%D0%BE%D0%BC%D1%87%D0%B5%D1%82%D0%B0%D1%82%D0%B0-%D0%BE%D1%82-%D0%BD%D0%B8%D0%BA%D0%B5%D0%BB-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD-%D0%BA%D0%BE%D0%B9%D1%82%D0%BE-%D0%B4%D0%BE%D0%BD%D0%B5%D1%81%D0%B5-%D0%B2%D1%82%D0%BE%D1%80%D0%B8-%D0%BF%D1%83%D0%BB%D0%B8%D1%86%D1%8A%D1%80-%D0%BD%D0%B0-%D0%BA%D0%BE%D0%BB%D1%81%D1%8A%D0%BD-%D1%83%D0%B0%D0%B9%D1%82%D1%85%D0%B5%D0%B4.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Момчетата от Никел, Колсън Уайтхед, Лист, нова книга, роман",
+            "date": "2021-04-21 00:00:00 +0000",
+            "image": "/Uploads/nickel_boys_cover.jpg",
+            "author": "Antonia",
+            "subtitle": "&quot;Фабрики за болка&quot; – така американският писател Колсън Уайтхед нарича поправителните училища в САЩ. Едно от тях е в центъра на романа му"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "За &quot;Неудобството на вечерта&quot;, в което се проявява истинската ни същност",
             "url": "/bookreviews/2021/04/20/09-58-%D0%B7%D0%B0-%D0%BD%D0%B5%D1%83%D0%B4%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%BE%D1%82%D0%BE-%D0%BD%D0%B0-%D0%B2%D0%B5%D1%87%D0%B5%D1%80%D1%82%D0%B0-%D0%B2-%D0%BA%D0%BE%D0%B5%D1%82%D0%BE-%D1%81%D0%B5-%D0%BF%D1%80%D0%BE%D1%8F%D0%B2%D1%8F%D0%B2%D0%B0-%D0%B8%D1%81%D1%82%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B8-%D1%81%D1%8A%D1%89%D0%BD%D0%BE%D1%81%D1%82.html",
             "category": "bookreviews",
