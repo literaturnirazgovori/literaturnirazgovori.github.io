25f70f6227463dc30cad34ca132ce94ce34911fc
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-05-11 07:05:41 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Доброволецът&quot; - историята на едно немислимо човеколюбие",
+            "url": "/bookreviews/2021/05/11/10-04-%D0%B4%D0%BE%D0%B1%D1%80%D0%BE%D0%B2%D0%BE%D0%BB%D0%B5%D1%86%D1%8A%D1%82-%D0%B8%D0%B7%D1%83%D0%BC%D0%B8%D1%82%D0%B5%D0%BB%D0%BD%D0%B0%D1%82%D0%B0-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%BD%D0%B0-%D0%BF%D0%BE%D0%BB%D1%81%D0%BA%D0%B8%D1%8F-%D0%B3%D0%B5%D1%80%D0%BE%D0%B9-%D0%B2%D0%B8%D1%82%D0%BE%D0%BB%D0%B4-%D0%BF%D0%B8%D0%BB%D0%B5%D1%86%D0%BA%D0%B8.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Джак Феъруедър, Доброволецът, документална, рецензия, Колибри",
+            "date": "2021-05-11 00:00:00 +0000",
+            "image": "/Uploads/dobrovolеtsat.jpg",
+            "author": "Antonia",
+            "subtitle": "Романът на Джак Феъруедър, който разказва за полския герой Витолд Пилецки, попада в графата на сериозните документални четива за &quot;Аушвиц&quot; и е отличена с престижната британска награда &quot;Коста&quot;"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;Край никой път&quot; на Мия Коту - за чудесата в тихото предградие на битието",
             "url": "/bookreviews/2021/04/29/13-01-%D0%BA%D1%80%D0%B0%D0%B9-%D0%BD%D0%B8%D0%BA%D0%BE%D0%B9-%D0%BF%D1%8A%D1%82-%D0%BD%D0%B0-%D0%BC%D0%B8%D1%8F-%D0%BA%D0%BE%D1%82%D1%83-%D0%B7%D0%B0-%D1%87%D1%83%D0%B4%D0%B5%D1%81%D0%B0%D1%82%D0%B0-%D0%B2-%D1%82%D0%B8%D1%85%D0%BE%D1%82%D0%BE-%D0%BF%D1%80%D0%B5%D0%B4%D0%B3%D1%80%D0%B0%D0%B4%D0%B8%D0%B5-%D0%BD%D0%B0-%D0%B1%D0%B8%D1%82%D0%B8%D0%B5%D1%82%D0%BE.html",
             "category": "bookreviews",
@@ -15,8 +31,6 @@ var search_json_index = [
         ,
         
     
-  
-      
   
           
         {
