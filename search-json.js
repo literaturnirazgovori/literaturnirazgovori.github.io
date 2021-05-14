647ce4b9ef1adce2f620df3d331130d8f9824401
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-05-14 19:05:45 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Колонката на Георги Цанков: Мия Коту, Роберт Зееталер, Цруя Шалев, Деляна Манева, Катерина Хапсали и още",
+            "url": "/kolonkata-na-georgi-cankov/2021/05/14/19-03-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D0%BC%D0%B8%D1%8F-%D0%BA%D0%BE%D1%82%D1%83-%D1%80%D0%BE%D0%B1%D0%B5%D1%80%D1%82-%D0%B7%D0%B5%D0%B5%D1%82%D0%B0%D0%BB%D0%B5%D1%80-%D1%86%D1%80%D1%83%D1%8F-%D1%88%D0%B0%D0%BB%D0%B5%D0%B2-%D0%B4%D0%B5%D0%BB%D1%8F%D0%BD%D0%B0-%D0%BC%D0%B0%D0%BD%D0%B5%D0%B2%D0%B0-%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B0-%D1%85%D0%B0%D0%BF%D1%81%D0%B0%D0%BB%D0%B8-%D0%B8-%D0%BE%D1%89%D0%B5.html",
+            "category": "kolonkata-na-georgi-cankov",
+            "category2": "kolonkata-na-georgi-cankov",
+            "tags": "Филипа Грегъри,  Роберт Зееталер, Катерина Хапсали, Иван Маразов, Деляна Манева, Цруя Шалев, Емили Дикинсън, Мия Коту",
+            "date": "2021-05-14 00:00:00 +0000",
+            "image": "/Uploads/kgt14may.jpg",
+            "author": "Guest",
+            "subtitle": "&quot;Не ми се сърдете, ако не откриете някое от любимите си нови заглавия – не си мислете, че не съм го видял. Просто, след като забавих ритъма, по-трудно подреждам приоритетите&quot;, пише критикът"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;Доброволецът&quot; - историята на едно немислимо човеколюбие",
             "url": "/bookreviews/2021/05/11/10-04-%D0%B4%D0%BE%D0%B1%D1%80%D0%BE%D0%B2%D0%BE%D0%BB%D0%B5%D1%86%D1%8A%D1%82-%D0%B8%D0%B7%D1%83%D0%BC%D0%B8%D1%82%D0%B5%D0%BB%D0%BD%D0%B0%D1%82%D0%B0-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%BD%D0%B0-%D0%BF%D0%BE%D0%BB%D1%81%D0%BA%D0%B8%D1%8F-%D0%B3%D0%B5%D1%80%D0%BE%D0%B9-%D0%B2%D0%B8%D1%82%D0%BE%D0%BB%D0%B4-%D0%BF%D0%B8%D0%BB%D0%B5%D1%86%D0%BA%D0%B8.html",
             "category": "bookreviews",
