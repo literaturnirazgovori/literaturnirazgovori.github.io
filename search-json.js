c0aee74a0efab7075075fd0326520a93bc714fd1
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-06-03 09:07:12 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Тракийски легенди&quot; на Кристина Димитрова - един по-различен урок по история (откъс)",
+            "url": "/newbooks/2021/06/03/09-05-%D1%82%D1%80%D0%B0%D0%BA%D0%B8%D0%B9%D1%81%D0%BA%D0%B8-%D0%BB%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B8-%D0%BD%D0%B0-%D0%BA%D1%80%D0%B8%D1%81%D1%82%D0%B8%D0%BD%D0%B0-%D0%B4%D0%B8%D0%BC%D0%B8%D1%82%D1%80%D0%BE%D0%B2%D0%B0-%D0%B5%D0%B4%D0%B8%D0%BD-%D0%BF%D0%BE-%D1%80%D0%B0%D0%B7%D0%BB%D0%B8%D1%87%D0%B5%D0%BD-%D1%83%D1%80%D0%BE%D0%BA-%D0%BF%D0%BE-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F-%D0%BE%D1%82%D0%BA%D1%8A%D1%81.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Кристина Димитрова, Тракийски легенди, Софтпрес, нова книга",
+            "date": "2021-06-03 00:00:00 +0000",
+            "image": "/Uploads/pages-from-trakiiski_legendi.jpg",
+            "author": "Antonia",
+            "subtitle": "ова е втората нейна книга, която излиза с логото на &quot;Софтпрес&quot; след &quot;Български митични създания&quot;, като този път Кристина Димитрова влиза в ролята както на автор, така и на талантлив илюстратор"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;Нас, които ни няма&quot; - новият роман на Антония Апостолова",
             "url": "/newbooks/2021/06/02/09-58-%D0%BD%D0%B0%D1%81-%D0%BA%D0%BE%D0%B8%D1%82%D0%BE-%D0%BD%D0%B8-%D0%BD%D1%8F%D0%BC%D0%B0-%D0%BD%D0%BE%D0%B2%D0%B8%D1%8F%D1%82-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD-%D0%BD%D0%B0-%D0%B0%D0%BD%D1%82%D0%BE%D0%BD%D0%B8%D1%8F-%D0%B0%D0%BF%D0%BE%D1%81%D1%82%D0%BE%D0%BB%D0%BE%D0%B2%D0%B0.html",
             "category": "newbooks",
