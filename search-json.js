329925331cc9d6c3cd7f7bcdee3679e6bc04aa68
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-07-27 07:44:32 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Мъдростта на детството в &quot;Момичето, което предсказваше миналото&quot;, или за миналото, направено от бъдеще",
+            "url": "/bookreviews/2021/07/27/10-42-%D0%BC%D1%8A%D0%B4%D1%80%D0%BE%D1%81%D1%82%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B4%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%BE%D1%82%D0%BE-%D0%B2-%D0%BC%D0%BE%D0%BC%D0%B8%D1%87%D0%B5%D1%82%D0%BE-%D0%BA%D0%BE%D0%B5%D1%82%D0%BE-%D0%BF%D1%80%D0%B5%D0%B4%D1%81%D0%BA%D0%B0%D0%B7%D0%B2%D0%B0%D1%88%D0%B5-%D0%BC%D0%B8%D0%BD%D0%B0%D0%BB%D0%BE%D1%82%D0%BE.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Момичето което предсказваше миналото, Красимир Димовски, Хермес, рецензия, разкази",
+            "date": "2021-07-27 00:00:00 +0000",
+            "image": "/Uploads/momichetokoetopredskazvashe.jpg",
+            "author": "Antonia",
+            "subtitle": "Именно чрез &quot;представянето&quot; се случва осмислянето на този непознат и привидно непонятен възрастен свят - смисълът, който децата му придават, се мести между властта на буквалното и мощта на всевъзможното"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Колонката на Георги Цанков: Световна поезия - Песоа, Роденбах, Верхарен, Сологуб (част 1)",
             "url": "/kolonkata-na-georgi-cankov/2021/07/26/08-45-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%81%D0%B2%D0%B5%D1%82%D0%BE%D0%B2%D0%BD%D0%B0-%D0%BF%D0%BE%D0%B5%D0%B7%D0%B8%D1%8F-%D0%BF%D0%B5%D1%81%D0%BE%D0%B0-%D1%80%D0%BE%D0%B4%D0%B5%D0%BD%D0%B1%D0%B0%D1%85-%D0%B2%D0%B5%D1%80%D1%85%D0%B0%D1%80%D0%B5%D0%BD-%D1%81%D0%BE%D0%BB%D0%BE%D0%B3%D1%83%D0%B1-%D1%87%D0%B0%D1%81%D1%82-1.html",
             "category": "kolonkata-na-georgi-cankov",
@@ -31,8 +47,6 @@ var search_json_index = [
         ,
         
     
-  
-      
   
           
         {
