20609f07108c8d2fcec629a7480b5455a6598806
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-09-21 07:22:22 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -1,6 +1,20 @@
 var search_json_index = [
     
-      
+          
+        {
+            "title": "Колонката на Георги Цанков: Фланьорстване из &quot;Спомнена София&quot; и куп нови книги за четене",
+            "url": "/kolonkata-na-georgi-cankov/2021/09/20/09-41-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%84%D0%BB%D0%B0%D0%BD%D1%8C%D0%BE%D1%80%D1%81%D1%82%D0%B2%D0%B0%D0%BD%D0%B5-%D0%B8%D0%B7-%D1%81%D0%BF%D0%BE%D0%BC%D0%BD%D0%B5%D0%BD%D0%B0-%D1%81%D0%BE%D1%84%D0%B8%D1%8F-%D0%B8-%D0%BA%D1%83%D0%BF-%D0%BD%D0%BE%D0%B2%D0%B8-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D0%B7%D0%B0-%D1%87%D0%B5%D1%82%D0%B5%D0%BD%D0%B5.html",
+            "category": "kolonkata-na-georgi-cankov",
+            "category2": "kolonkata-na-georgi-cankov",
+            "tags": "Тони Николов, Спомнена София, Колонката на Георги Цанков",
+            "date": "2021-09-20 00:00:00 +0000",
+            "image": "/Uploads/kgt21sept.jpg",
+            "author": "Guest",
+            "subtitle": "Тони Николов е от онези издигнати интелектуалци, които ясно осъзнават, че „нашите стъпки се оказват несъзнателно продължение на стъпките, които други хора някога са поставяли по същите тези улици“"
+        } 
+        ,
+        
+    
   
           
         {
