a68dec18a375fc85667f1509e88c1f61d3c47ba4
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-11-17 10:11:17 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Невена Митрополитска: Да имаш избор не е лесно, но е за предпочитане пред това да нямаш",
+            "url": "/interviews/2021/11/17/10-09-%D0%BD%D0%B5%D0%B2%D0%B5%D0%BD%D0%B0-%D0%BC%D0%B8%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D1%81%D0%BA%D0%B0-%D0%B4%D0%B0-%D0%B8%D0%BC%D0%B0%D1%88-%D0%B8%D0%B7%D0%B1%D0%BE%D1%80-%D0%BD%D0%B5-%D0%B5-%D0%BB%D0%B5%D1%81%D0%BD%D0%BE-%D0%BD%D0%BE-%D0%B5-%D0%B7%D0%B0-%D0%BF%D1%80%D0%B5%D0%B4%D0%BF%D0%BE%D1%87%D0%B8%D1%82%D0%B0%D0%BD%D0%B5-%D0%BF%D1%80%D0%B5%D0%B4-%D1%82%D0%BE%D0%B2%D0%B0-%D0%B4%D0%B0-%D0%BD%D1%8F%D0%BC%D0%B0%D1%88.html",
+            "category": "interviews",
+            "category2": "",
+            "tags": "По релсите,  Невена Митрополитска, Жанет 45, роман, интервю",
+            "date": "2021-11-17 00:00:00 +0000",
+            "image": "/Uploads/nevenamitropolitska.jpg",
+            "author": "Antonia",
+            "subtitle": "Определено изпитвах страх да навляза в тази тема. Дори и сега се опасявам, че някой ще ми каже: „Ти не можеш да ни разбереш, ти нямаш право да пишеш за това!“"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Откъси от новия роман на Невена Митрополитска &quot;По релсите&quot;",
             "url": "/newbooks/2021/11/02/11-00-%D0%BE%D1%82%D0%BA%D1%8A%D1%81%D0%B8-%D0%BE%D1%82-%D0%BD%D0%BE%D0%B2%D0%B8%D1%8F-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD-%D0%BD%D0%B0-%D0%BD%D0%B5%D0%B2%D0%B5%D0%BD%D0%B0-%D0%BC%D0%B8%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D1%81%D0%BA%D0%B0-%D0%BF%D0%BE-%D1%80%D0%B5%D0%BB%D1%81%D0%B8%D1%82%D0%B5.html",
             "category": "newbooks",
