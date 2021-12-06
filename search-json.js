b70e5eed2f47c8c19198ac3830feeef18c347f3e
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-12-06 12:36:07 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Война и мир&quot; в ново луксозно четиритомно издание с илюстрации на Теодор Ушев",
+            "url": "/newbooks/2021/12/06/12-33-%D0%B2%D0%BE%D0%B9%D0%BD%D0%B0-%D0%B8-%D0%BC%D0%B8%D1%80-%D0%B2-%D0%BD%D0%BE%D0%B2%D0%BE-%D0%BB%D1%83%D0%BA%D1%81%D0%BE%D0%B7%D0%BD%D0%BE-%D1%87%D0%B5%D1%82%D0%B8%D1%80%D0%B8%D1%82%D0%BE%D0%BC%D0%BD%D0%BE-%D0%B8%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D1%81-%D0%B8%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BE%D0%B4%D0%BE%D1%80-%D1%83%D1%88%D0%B5%D0%B2.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Война и мир, Толстой, нова книга, Кръг",
+            "date": "2021-12-06 00:00:00 +0000",
+            "image": "/Uploads/voyna-i-mir-tom-1-luksozno-izdanie-30.jpg",
+            "author": "Antonia",
+            "subtitle": "&quot;Война и мир&quot; е забележителна смесица между реализъм, историческа проза и философия. За изданието е използван класическият превод на Константин Константинов от 1957 г."
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Теодора Лалова: Има някаква чудесна свобода в това да възприемаш обикновеното като събитие",
             "url": "/interviews/2021/12/03/11-39-%D1%82%D0%B5%D0%BE%D0%B4%D0%BE%D1%80%D0%B0-%D0%BB%D0%B0%D0%BB%D0%BE%D0%B2%D0%B0-%D0%B8%D0%BC%D0%B0-%D0%BD%D1%8F%D0%BA%D0%B0%D0%BA%D0%B2%D0%B0-%D1%87%D1%83%D0%B4%D0%B5%D1%81%D0%BD%D0%B0-%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B4%D0%B0-%D0%B2-%D1%82%D0%BE%D0%B2%D0%B0-%D0%B4%D0%B0-%D0%B2%D1%8A%D0%B7%D0%BF%D1%80%D0%B8%D0%B5%D0%BC%D0%B0%D1%88-%D0%BE%D0%B1%D0%B8%D0%BA%D0%BD%D0%BE%D0%B2%D0%B5%D0%BD%D0%BE%D1%82%D0%BE-%D0%BA%D0%B0%D1%82%D0%BE-%D1%81%D1%8A%D0%B1%D0%B8%D1%82%D0%B8%D0%B5.html",
             "category": "interviews",
