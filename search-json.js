b50d932fceb8a1a35b284008481aaaf4a09af119
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-12-09 12:02:52 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Колонката на Георги Цанков: Разходка из Коледния панаир на книгата (част 1)",
+            "url": "/kolonkata-na-georgi-cankov/2021/12/09/12-00-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%80%D0%B0%D0%B7%D1%85%D0%BE%D0%B4%D0%BA%D0%B0-%D0%B8%D0%B7-%D0%BA%D0%BE%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D1%8F-%D0%BF%D0%B0%D0%BD%D0%B0%D0%B8%D1%80-%D0%BD%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0%D1%82%D0%B0-%D1%87%D0%B0%D1%81%D1%82-1.html",
+            "category": "kolonkata-na-georgi-cankov",
+            "category2": "kolonkata-na-georgi-cankov",
+            "tags": "Колонката на Георги Цанков, обзор, панаир",
+            "date": "2021-12-09 00:00:00 +0000",
+            "image": "/Uploads/kgt09dec.jpg",
+            "author": "Guest",
+            "subtitle": "Има книги и издателства, за които ще говоря отделно и подробно след празниците. Сега искам да ви предложа безценни съкровища под елхата, моя избор на безценните издания в края на годината"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;По релсите&quot; на Невена Митрополитска – за красотата, и след мига на разпада",
             "url": "/bookreviews/2021/12/09/10-43-%D0%BF%D0%BE-%D1%80%D0%B5%D0%BB%D1%81%D0%B8%D1%82%D0%B5-%D0%BD%D0%B0-%D0%BD%D0%B5%D0%B2%D0%B5%D0%BD%D0%B0-%D0%BC%D0%B8%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D1%81%D0%BA%D0%B0-%D0%B7%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%BE%D1%82%D0%B0%D1%82%D0%B0-%D0%B8-%D1%81%D0%BB%D0%B5%D0%B4-%D0%BC%D0%B8%D0%B3%D0%B0-%D0%BD%D0%B0-%D1%80%D0%B0%D0%B7%D0%BF%D0%B0%D0%B4%D0%B0.html",
             "category": "bookreviews",
