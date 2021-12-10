023b341ed97cce11291aa14d193343e7b19dbd1d
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-12-10 09:34:05 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Нещотърсач&quot; събира 44 предмета от близо и далеч и техните истории",
+            "url": "/newbooks/2021/12/10/09-31-%D0%BD%D0%B5%D1%89%D0%BE%D1%82%D1%8A%D1%80%D1%81%D0%B0%D1%87-%D1%81%D1%8A%D0%B1%D0%B8%D1%80%D0%B0-44-%D0%BF%D1%80%D0%B5%D0%B4%D0%BC%D0%B5%D1%82%D0%B0-%D0%BE%D1%82-%D0%B1%D0%BB%D0%B8%D0%B7%D0%BE-%D0%B8-%D0%B4%D0%B0%D0%BB%D0%B5%D1%87-%D0%B8-%D1%82%D0%B5%D1%85%D0%BD%D0%B8%D1%82%D0%B5-%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D0%B8.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Екатерина Петрова, Жанет 45, Нещотърсач, нова книга",
+            "date": "2021-12-10 00:00:00 +0000",
+            "image": "/Uploads/neshtotarsach.jpg",
+            "author": "Antonia",
+            "subtitle": "Разказвайки историите им, Екатерина Петрова всъщност разказва и за местата, откъдето те произлизат, като ги поставя в по-широк езиков, културен, исторически или географски контекст"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Колонката на Георги Цанков: Разходка из Коледния панаир на книгата (част 1)",
             "url": "/kolonkata-na-georgi-cankov/2021/12/09/12-00-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%80%D0%B0%D0%B7%D1%85%D0%BE%D0%B4%D0%BA%D0%B0-%D0%B8%D0%B7-%D0%BA%D0%BE%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D1%8F-%D0%BF%D0%B0%D0%BD%D0%B0%D0%B8%D1%80-%D0%BD%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0%D1%82%D0%B0-%D1%87%D0%B0%D1%81%D1%82-1.html",
             "category": "kolonkata-na-georgi-cankov",
