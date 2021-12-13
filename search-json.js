3dd8ab8840db1610b24eae8ebbb291986b37c3cb
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-12-13 11:55:28 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Колонката на Георги Цанков: Разходка из Коледния панаир на книгата (част 3)",
+            "url": "/kolonkata-na-georgi-cankov/2021/12/13/11-53-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%80%D0%B0%D0%B7%D1%85%D0%BE%D0%B4%D0%BA%D0%B0-%D0%B8%D0%B7-%D0%BA%D0%BE%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D1%8F-%D0%BF%D0%B0%D0%BD%D0%B0%D0%B8%D1%80-%D0%BD%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0%D1%82%D0%B0-%D1%87%D0%B0%D1%81%D1%82-3.html",
+            "category": "kolonkata-na-georgi-cankov",
+            "category2": "kolonkata-na-georgi-cankov",
+            "tags": "Колонката на Георги Цанков",
+            "date": "2021-12-13 00:00:00 +0000",
+            "image": "/Uploads/kgt13dec.jpg",
+            "author": "Guest",
+            "subtitle": "Този път разходката ни отнася до щандовете на издателствата &quot;Жар, &quot;Прозорец&quot;, &quot;Кръгозор&quot;, &quot;Емас&quot;, &quot;Вакон&quot;"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Колонката на Георги Цанков: Разходка из Коледния панаир на книгата (част 2)",
             "url": "/kolonkata-na-georgi-cankov/2021/12/10/17-42-%D0%BA%D0%BE%D0%BB%D0%BE%D0%BD%D0%BA%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%B3%D0%B5%D0%BE%D1%80%D0%B3%D0%B8-%D1%86%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D1%80%D0%B0%D0%B7%D1%85%D0%BE%D0%B4%D0%BA%D0%B0-%D0%B8%D0%B7-%D0%BA%D0%BE%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D1%8F-%D0%BF%D0%B0%D0%BD%D0%B0%D0%B8%D1%80-%D0%BD%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0%D1%82%D0%B0-%D1%87%D0%B0%D1%81%D1%82-2.html",
             "category": "kolonkata-na-georgi-cankov",
