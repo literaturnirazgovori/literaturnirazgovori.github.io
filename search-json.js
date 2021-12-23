ff2d05f424b82674f5369f09d2c3a10701c419b3
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2021-12-23 08:47:53 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -3,11 +3,11 @@ var search_json_index = [
           
         {
             "title": "Никола Петров: Грозно е онова, което все още не възприемаме за красиво",
-            "url": "/interviews/2021/12/21/18-37-%D0%BD%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0-%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2-%D0%B3%D1%80%D0%BE%D0%B7%D0%BD%D0%BE%D1%82%D0%BE-%D0%B5-%D0%BE%D0%BD%D0%BE%D0%B2%D0%B0-%D0%BA%D0%BE%D0%B5%D1%82%D0%BE-%D0%B2%D1%81%D0%B5-%D0%BE%D1%89%D0%B5-%D0%BD%D0%B5-%D0%B2%D1%8A%D0%B7%D0%BF%D1%80%D0%B8%D0%B5%D0%BC%D0%B0%D0%BC%D0%B5-%D0%B7%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D0%BE.html",
+            "url": "/interviews/2021/12/23/10-39-%D0%BD%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0-%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2-%D0%B3%D1%80%D0%BE%D0%B7%D0%BD%D0%BE%D1%82%D0%BE-%D0%B5-%D0%BE%D0%BD%D0%BE%D0%B2%D0%B0-%D0%BA%D0%BE%D0%B5%D1%82%D0%BE-%D0%B2%D1%81%D0%B5-%D0%BE%D1%89%D0%B5-%D0%BD%D0%B5-%D0%B2%D1%8A%D0%B7%D0%BF%D1%80%D0%B8%D0%B5%D0%BC%D0%B0%D0%BC%D0%B5-%D0%B7%D0%B0-%D0%BA%D1%80%D0%B0%D1%81%D0%B8%D0%B2%D0%BE.html",
             "category": "interviews",
             "category2": "",
             "tags": "Никола Петров, Издателство за поезия ДА, поезия, интервю, Не са чудовища",
-            "date": "2021-12-21 00:00:00 +0000",
+            "date": "2021-12-23 00:00:00 +0000",
             "image": "/Uploads/nikolapetrov.jpg",
             "author": "Antonia",
             "subtitle": "Литературата едва ли е възможна без надежда. Абсолютната безнадеждност би била скучна, тя е като пълното щастие, не може да породи конфликт"
