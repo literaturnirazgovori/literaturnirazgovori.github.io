cef4314eaba889b0d2554ad0e99a09a4b1c3203e
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-02-10 17:38:35 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,7 +2,7 @@ var search_json_index = [
     
           
         {
-            "title": "Майки и дъщери: Феранте срещу Джиленхал. И възможният български отговор в &quot;Направени от вина&quot;",
+            "title": "Майки и дъщери: Феранте или Джиленхал. И българските жени в &quot;Направени от вина&quot;",
             "url": "/bookreviews/2022/02/10/19-16-%D1%84%D0%B5%D1%80%D0%B0%D0%BD%D1%82%D0%B5-%D1%81%D1%80%D0%B5%D1%89%D1%83-%D0%B4%D0%B6%D0%B8%D0%BB%D0%B5%D0%BD%D1%85%D0%B0%D0%BB-%D0%BC%D0%B0%D0%B9%D0%BA%D0%B8-%D0%B8-%D0%B4%D1%8A%D1%89%D0%B5%D1%80%D0%B8-%D0%B2-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD%D0%B0-%D0%BD%D0%B5%D0%BF%D0%BE%D0%B7%D0%BD%D0%B0%D1%82%D0%B0%D1%82%D0%B0-%D0%B4%D1%8A%D1%89%D0%B5%D1%80%D1%8F-%D0%B8-%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%D1%82%D0%B0-%D0%BC%D1%83.html",
             "category": "bookreviews",
             "category2": "",
