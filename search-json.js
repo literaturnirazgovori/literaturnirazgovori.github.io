10d432787ec214e53546900c33e0c863fbae3c8c
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-04-07 07:12:48 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,7 +2,7 @@ var search_json_index = [
     
           
         {
-            "title": "&quot;Звезди под клепачите&quot; на Николай Терзийски - за любовта отвъд времето и непринадлежността към собствения свят",
+            "title": "&quot;Звезди под клепачите&quot; на Николай Терзийски - за любовта като липса на смърт",
             "url": "/bookreviews/2022/04/07/06-30-%D0%B7%D0%B2%D0%B5%D0%B7%D0%B4%D0%B8-%D0%BF%D0%BE%D0%B4-%D0%BA%D0%BB%D0%B5%D0%BF%D0%B0%D1%87%D0%B8%D1%82%D0%B5-%D0%BD%D0%B0-%D0%BD%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9-%D1%82%D0%B5%D1%80%D0%B7%D0%B8%D0%B9%D1%81%D0%BA%D0%B8-%D0%B7%D0%B0-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2%D1%82%D0%B0-%D0%BE%D1%82%D0%B2%D1%8A%D0%B4-%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D1%82%D0%BE-%D0%B8-%D0%BD%D0%B5%D0%BF%D1%80%D0%B8%D0%BD%D0%B0%D0%B4%D0%BB%D0%B5%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%82%D0%B0-%D0%BA%D1%8A%D0%BC-%D1%81%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%B8%D1%8F-%D1%81%D0%B2%D1%8F%D1%82.html",
             "category": "bookreviews",
             "category2": "",
