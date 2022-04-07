516f56f1055e8115514a693a3b46186c2c0d3a54
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-04-07 06:33:06 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Звезди под клепачите&quot; на Николай Терзийски - за любовта отвъд времето и непринадлежността към собствения свят",
+            "url": "/bookreviews/2022/04/07/06-30-%D0%B7%D0%B2%D0%B5%D0%B7%D0%B4%D0%B8-%D0%BF%D0%BE%D0%B4-%D0%BA%D0%BB%D0%B5%D0%BF%D0%B0%D1%87%D0%B8%D1%82%D0%B5-%D0%BD%D0%B0-%D0%BD%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9-%D1%82%D0%B5%D1%80%D0%B7%D0%B8%D0%B9%D1%81%D0%BA%D0%B8-%D0%B7%D0%B0-%D0%BB%D1%8E%D0%B1%D0%BE%D0%B2%D1%82%D0%B0-%D0%BE%D1%82%D0%B2%D1%8A%D0%B4-%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D1%82%D0%BE-%D0%B8-%D0%BD%D0%B5%D0%BF%D1%80%D0%B8%D0%BD%D0%B0%D0%B4%D0%BB%D0%B5%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%82%D0%B0-%D0%BA%D1%8A%D0%BC-%D1%81%D0%BE%D0%B1%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%B8%D1%8F-%D1%81%D0%B2%D1%8F%D1%82.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Николай Терзойски , Жанет 45, Звезди под клепачите, рецензия",
+            "date": "2022-04-07 00:00:00 +0000",
+            "image": "/Uploads/zcezdi-pod-klepachite-janet-45.jpg",
+            "author": "Antonia",
+            "subtitle": "Книгата проследява живота и семейното минало на героите в три любовни истории от три периода, които представляват паралелни структури, отразени една в друга"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Кирил Нейков за &quot;Dirty Side&quot;: Времето не е враг, харесвам този процес на деструктивно обновление",
             "url": "/interviews/2022/03/29/07-49-%D0%BA%D0%B8%D1%80%D0%B8%D0%BB-%D0%BD%D0%B5%D0%B9%D0%BA%D0%BE%D0%B2-%D0%B7%D0%B0-dirty-side-%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D1%82%D0%BE-%D0%BD%D0%B5-%D0%B5-%D0%B2%D1%80%D0%B0%D0%B3-%D1%85%D0%B0%D1%80%D0%B5%D1%81%D0%B2%D0%B0%D0%BC-%D1%82%D0%BE%D0%B7%D0%B8-%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81-%D0%BD%D0%B0-%D0%B4%D0%B5%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE-%D0%BE%D0%B1%D0%BD%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5.html",
             "category": "interviews",
