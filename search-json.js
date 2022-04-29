fdae4774e027941267883203b114193c31df2519
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-04-29 10:52:19 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Самотен мъж&quot; на Кристофър Ишъруд - най-после на български",
+            "url": "/newbooks/2022/04/29/10-49-%D1%81%D0%B0%D0%BC%D0%BE%D1%82%D0%B5%D0%BD-%D0%BC%D1%8A%D0%B6-%D0%BD%D0%B0-%D0%BA%D1%80%D0%B8%D1%81%D1%82%D0%BE%D1%84%D1%8A%D1%80-%D0%B8%D1%88%D1%8A%D1%80%D1%83%D0%B4-%D0%BD%D0%B0%D0%B9-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5-%D0%BD%D0%B0-%D0%B1%D1%8A%D0%BB%D0%B3%D0%B0%D1%80%D1%81%D0%BA%D0%B8.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Кристофът Ишъруд, Самотен мъж, роман, Лист, нова книга",
+            "date": "2022-04-29 00:00:00 +0000",
+            "image": "/Uploads/samotenmyzh.jpg",
+            "author": "Antonia",
+            "subtitle": "Книгата от 1964 г. се смята за най-доброто произведение на писателя вдъхновител на Холивуд и света извън него. Прводът е на Иглика Василева"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Вангел Имреоров: Всяка нова книга, която пиша, е къща в миналото",
             "url": "/interviews/2022/04/19/07-32-%D0%B2%D0%B0%D0%BD%D0%B3%D0%B5%D0%BB-%D0%B8%D0%BC%D1%80%D0%B5%D0%BE%D1%80%D0%BE%D0%B2-%D0%B2%D1%81%D1%8F%D0%BA%D0%B0-%D0%BD%D0%BE%D0%B2%D0%B0-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0-%D0%BA%D0%BE%D1%8F%D1%82%D0%BE-%D0%BF%D0%B8%D1%88%D0%B0-%D0%B5-%D0%BA%D1%8A%D1%89%D0%B0-%D0%B2-%D0%BC%D0%B8%D0%BD%D0%B0%D0%BB%D0%BE%D1%82%D0%BE.html",
             "category": "interviews",
