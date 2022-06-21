6af2656b0952ff907e83aeb6685471846caa7e43
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-06-21 11:52:15 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "ss",
+            "url": "/interviews/2022/06/21/11-50-ss.html",
+            "category": "interviews",
+            "category2": "",
+            "tags": "Нинко Кирилов, Атера, Георги Георгиев, И други дупки , интервю",
+            "date": "2022-06-21 00:00:00 +0000",
+            "image": "/Uploads/ninkoigeorgi.jpg",
+            "author": "Antonia",
+            "subtitle": "ss"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Николай Терзийски: Бъдещето е измисленото от човека",
             "url": "/interviews/2022/06/14/11-02-%D0%BD%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9-%D1%82%D0%B5%D1%80%D0%B7%D0%B8%D0%B9%D1%81%D0%BA%D0%B8-%D0%B1%D1%8A%D0%B4%D0%B5%D1%89%D0%B5%D1%82%D0%BE-%D0%B5-%D0%B8%D0%B7%D0%BC%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%BE%D1%82%D0%BE-%D0%BE%D1%82-%D1%87%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0.html",
             "category": "interviews",
