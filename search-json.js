7088026434f4f618040bf29f60e99c37ec863b1a
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-08-18 07:55:05 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Йордан Славейков: Там, отвъд, ме чака голямото ми кръвно-празнично семейство",
+            "url": "/interviews/2022/08/18/07-52-%D0%B9%D0%BE%D1%80%D0%B4%D0%B0%D0%BD-%D1%81%D0%BB%D0%B0%D0%B2%D0%B5%D0%B9%D0%BA%D0%BE%D0%B2-%D1%82%D0%B0%D0%BC-%D0%BE%D1%82%D0%B2%D1%8A%D0%B4-%D0%BC%D0%B5-%D1%87%D0%B0%D0%BA%D0%B0-%D0%B3%D0%BE%D0%BB%D1%8F%D0%BC%D0%BE%D1%82%D0%BE-%D0%BC%D0%B8-%D0%BA%D1%80%D1%8A%D0%B2%D0%BD%D0%BE-%D0%BF%D1%80%D0%B0%D0%B7%D0%BD%D0%B8%D1%87%D0%BD%D0%BE-%D1%81%D0%B5%D0%BC%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE.html",
+            "category": "interviews",
+            "category2": "",
+            "tags": "Йордан Славейков, Жанет 45, Празнично семейство, Последна стъпка, интервю",
+            "date": "2022-08-18 00:00:00 +0000",
+            "image": "/Uploads/yordanslaveykov.jpg",
+            "author": "Antonia",
+            "subtitle": "Когато пиша, си давам свободата не само да създавам света по мой образ и подобие, а се ползвам и от свободата да променям вече изживените си сценарии, да ги изигравам пак"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "&quot;Зелена безбрежност&quot; на Евгения факину - за силата на освобождаващата се жена",
             "url": "/bookreviews/2022/07/27/23-01-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%B0-%D0%B1%D0%B5%D0%B7%D0%B1%D1%80%D0%B5%D0%B6%D0%BD%D0%BE%D1%81%D1%82-%D0%BD%D0%B0-%D0%B5%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D1%8F-%D1%84%D0%B0%D0%BA%D0%B8%D0%BD%D1%83-%D0%B7%D0%B0-%D1%81%D0%B8%D0%BB%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-%D0%BE%D1%81%D0%B2%D0%BE%D0%B1%D0%BE%D0%B6%D0%B4%D0%B0%D0%B2%D0%B0%D1%89%D0%B0%D1%82%D0%B0-%D1%81%D0%B5-%D0%B6%D0%B5%D0%BD%D0%B0.html",
             "category": "bookreviews",
