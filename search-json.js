48e836df9d3d7867f9e34ebc80050e8dd5a63807
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-08-26 07:30:36 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "Иван Станков: Казват, че последна си отива надеждата. Боя се, че последна си отива болката",
+            "url": "/interviews/2022/08/26/07-28-%D0%B8%D0%B2%D0%B0%D0%BD-%D1%81%D1%82%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D0%BA%D0%B0%D0%B7%D0%B2%D0%B0%D1%82-%D1%87%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BD%D0%B0-%D1%81%D0%B8-%D0%BE%D1%82%D0%B8%D0%B2%D0%B0-%D0%BD%D0%B0%D0%B4%D0%B5%D0%B6%D0%B4%D0%B0%D1%82%D0%B0-%D0%B1%D0%BE%D1%8F-%D1%81%D0%B5-%D1%87%D0%B5-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BD%D0%B0-%D1%81%D0%B8-%D0%BE%D1%82%D0%B8%D0%B2%D0%B0-%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0%D1%82%D0%B0.html",
+            "category": "interviews",
+            "category2": "",
+            "tags": "Иван Станков, Късна смърт, Хермес, интервю",
+            "date": "2022-08-26 00:00:00 +0000",
+            "image": "/Uploads/ivanstankov.jpg",
+            "author": "Antonia",
+            "subtitle": "В литературен смисъл знанието е натрупана с четене височина, от която може да се види повече. Пишещият трябва дълго да се обучава в четене"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Йордан Славейков: Там, отвъд, ме чака голямото ми кръвно-празнично семейство",
             "url": "/interviews/2022/08/18/07-52-%D0%B9%D0%BE%D1%80%D0%B4%D0%B0%D0%BD-%D1%81%D0%BB%D0%B0%D0%B2%D0%B5%D0%B9%D0%BA%D0%BE%D0%B2-%D1%82%D0%B0%D0%BC-%D0%BE%D1%82%D0%B2%D1%8A%D0%B4-%D0%BC%D0%B5-%D1%87%D0%B0%D0%BA%D0%B0-%D0%B3%D0%BE%D0%BB%D1%8F%D0%BC%D0%BE%D1%82%D0%BE-%D0%BC%D0%B8-%D0%BA%D1%80%D1%8A%D0%B2%D0%BD%D0%BE-%D0%BF%D1%80%D0%B0%D0%B7%D0%BD%D0%B8%D1%87%D0%BD%D0%BE-%D1%81%D0%B5%D0%BC%D0%B5%D0%B9%D1%81%D1%82%D0%B2%D0%BE.html",
             "category": "interviews",
