f9d8179045fd93f8aa16ea45f4ee12fe80dc46f2
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-09-13 13:20:48 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "30 бисера от литературата на XX век обещани от издателство &quot;Колибри&quot;",
+            "url": "/newbooks/2022/09/13/13-18-30-%D0%B1%D0%B8%D1%81%D0%B5%D1%80%D0%B0-%D0%BE%D1%82-%D0%BB%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%82%D1%83%D1%80%D0%B0%D1%82%D0%B0-%D0%BD%D0%B0-xx-%D0%B2%D0%B5%D0%BA-%D0%BE%D0%B1%D0%B5%D1%89%D0%B0%D0%BD%D0%B8-%D0%BE%D1%82-%D0%B8%D0%B7%D0%B4%D0%B0%D1%82%D0%B5%D0%BB%D1%81%D1%82%D0%B2%D0%BE-%D0%BA%D0%BE%D0%BB%D0%B8%D0%B1%D1%80%D0%B8.html",
+            "category": "newbooks",
+            "category2": "",
+            "tags": "Колибри, класика, нова книга",
+            "date": "2022-09-13 00:00:00 +0000",
+            "image": "/Uploads/30classicbookscolibri.jpg",
+            "author": "Antonia",
+            "subtitle": "Поредицата включва нови издания, нови преводи, както и неиздавани досега важни творби на световни белетристи. Илюстрирана е от Иво Рафаилов и Теодор Ушев"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Никой не умира навреме - отворено писмо до Иван Станков за неговата &quot;Късна смърт&quot; ",
             "url": "/bookreviews/2022/09/06/19-09-%D0%BD%D0%B8%D0%BA%D0%BE%D0%B9-%D0%BD%D0%B5-%D1%83%D0%BC%D0%B8%D1%80%D0%B0-%D0%BD%D0%B0%D0%B2%D1%80%D0%B5%D0%BC%D0%B5-%D0%BE%D1%82%D0%B2%D0%BE%D1%80%D0%B5%D0%BD%D0%BE-%D0%BF%D0%B8%D1%81%D0%BC%D0%BE-%D0%B4%D0%BE-%D0%B8%D0%B2%D0%B0%D0%BD-%D1%81%D1%82%D0%B0%D0%BD%D0%BA%D0%BE%D0%B2-%D0%B7%D0%B0-%D0%BD%D0%B5%D0%B3%D0%BE%D0%B2%D0%B0%D1%82%D0%B0-%D0%BA%D1%8A%D1%81%D0%BD%D0%B0-%D1%81%D0%BC%D1%8A%D1%80%D1%82.html",
             "category": "bookreviews",
