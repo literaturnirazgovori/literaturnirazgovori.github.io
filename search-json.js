d89469656856279999b8dd694b5eafe73b679dd9
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-11-16 08:02:13 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,6 +2,22 @@ var search_json_index = [
     
           
         {
+            "title": "&quot;Да умре ли старецът или не&quot;, или Чавдар Ценов по Шекспир",
+            "url": "/bookreviews/2022/11/16/08-00-%D0%B4%D0%B0-%D1%83%D0%BC%D1%80%D0%B5-%D0%BB%D0%B8-%D1%81%D1%82%D0%B0%D1%80%D0%B5%D1%86%D1%8A%D1%82-%D0%B8%D0%BB%D0%B8-%D0%BD%D0%B5-%D0%B8%D0%BB%D0%B8-%D1%87%D0%B0%D0%B2%D0%B4%D0%B0%D1%80-%D1%86%D0%B5%D0%BD%D0%BE%D0%B2-%D0%BF%D0%BE-%D1%88%D0%B5%D0%BA%D1%81%D0%BF%D0%B8%D1%80.html",
+            "category": "bookreviews",
+            "category2": "",
+            "tags": "Старецът трябва да умре, Чавдар Ценов, Рива, рецензия, повест",
+            "date": "2022-11-16 00:00:00 +0000",
+            "image": "/Uploads/starecyttryabvadaumre.jpg",
+            "author": "Antonia",
+            "subtitle": "&quot;Старецът трябва да умре&quot; e повест за живеенето пред хоризонта на неизбежния сюжетен край - смъртта, и за невъзможността не само ние, но и самият Бог да редактира миналото"
+        } 
+        ,
+        
+    
+  
+          
+        {
             "title": "Диана Саватева: В непреживяното и неслучилото се също има опит",
             "url": "/interviews/2022/11/14/12-42-%D0%B4%D0%B8%D0%B0%D0%BD%D0%B0-%D1%81%D0%B0%D0%B2%D0%B0%D1%82%D0%B5%D0%B2%D0%B0-%D0%B2-%D0%BD%D0%B5%D0%BF%D1%80%D0%B5%D0%B6%D0%B8%D0%B2%D1%8F%D0%BD%D0%BE%D1%82%D0%BE-%D0%B8-%D0%BD%D0%B5%D1%81%D0%BB%D1%83%D1%87%D0%B8%D0%BB%D0%BE%D1%82%D0%BE-%D1%81%D0%B5-%D1%81%D1%8A%D1%89%D0%BE-%D0%B8%D0%BC%D0%B0-%D0%BE%D0%BF%D0%B8%D1%82-%D0%B8-%D0%B3%D0%BE%D1%80%D1%87%D0%B8%D0%B2-%D0%B8-%D0%B1%D0%B5%D0%B7%D1%86%D0%B5%D0%BD%D0%B5%D0%BD.html",
             "category": "interviews",
