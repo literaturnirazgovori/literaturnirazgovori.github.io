7b248e655ccfe56556a0232fc42d11d68a25a167
Alon Rotem (from Travis CI) <alrotem@gmail.com>
2022-11-16 08:56:27 +0000

    Deploy literaturnirazgovori/literaturnirazgovori.github.io to github.com/literaturnirazgovori/literaturnirazgovori.github.io.git:master

--- search-json.js
+++ search-json.js
@@ -2,7 +2,7 @@ var search_json_index = [
     
           
         {
-            "title": "&quot;Да умре ли старецът или не&quot;, или Чавдар Ценов по Шекспир",
+            "title": "&quot;Да умре ли старецът, или не&quot;, или Чавдар Ценов по Шекспир",
             "url": "/bookreviews/2022/11/16/08-00-%D0%B4%D0%B0-%D1%83%D0%BC%D1%80%D0%B5-%D0%BB%D0%B8-%D1%81%D1%82%D0%B0%D1%80%D0%B5%D1%86%D1%8A%D1%82-%D0%B8%D0%BB%D0%B8-%D0%BD%D0%B5-%D0%B8%D0%BB%D0%B8-%D1%87%D0%B0%D0%B2%D0%B4%D0%B0%D1%80-%D1%86%D0%B5%D0%BD%D0%BE%D0%B2-%D0%BF%D0%BE-%D1%88%D0%B5%D0%BA%D1%81%D0%BF%D0%B8%D1%80.html",
             "category": "bookreviews",
             "category2": "",
