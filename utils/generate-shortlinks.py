2187cf58670f0448b184fa87e5be37ce7c78c237
Alon Rotem <alrotem@gmail.com>
2024-12-26 15:16:19 +0200

    Short links, in progress

--- utils/generate-shortlinks.py
+++ utils/generate-shortlinks.py
@@ -16,11 +16,50 @@ print("=====generate-shortlinks.py================================")
 
 # All the changed files in this commit
 changed_files = os.environ["ALL_CHANGED_FILES"]
-print("changed_files:")
-print(changed_files)
-print("Loading json...")
 filenames = json.loads(changed_files.encode('utf-8').decode('unicode_escape'))
-for f in filenames:
-  print(f"Py script, changed file: {f}")
+files_changed_for_commit = 0
+
+for post_file_name in filenames:
+  # was a post changed?
+  if post_file_name.startswith("_posts"):
+    save_file = False
+    post_full_file_name = os.path.join(POSTS_DIR, post_file_name)
+    post_frontmatter = frontmatter.load(post_full_file_name)
+    
+    # is shortlink populated?
+    shortlink_url=""
+    if "shortlink" in post_frontmatter.keys():
+        shortlink_url = int(post_frontmatter["shortlink"])
+    
+    # if not populated, create a shortlink based on the filename
+    if shortlink_url == "":
+        filename_parts = re.search(r"(\d{4})\-(\d{2})\-(\d{2})\-(\d{2})\-(\d{2})\-([^\.]*)", post_file_name.replace(".md", ""))
+        if filename_parts and filename_parts.groups() and len(filename_parts.groups()) >= 6:
+            yer = filename_parts.groups()[0] # year
+            mon = filename_parts.groups()[1] # month
+            day = filename_parts.groups()[2] # day
+            hur = filename_parts.groups()[3] # hour
+            min = filename_parts.groups()[4] # minutes
+            ttl = filename_parts.groups()[5] # title (truncated to 5 chars)
+
+            shortlink_url=f"https://literaturnirazgovori.com/{yer}{mon}{day}{hur}{min}"
+
+    # make sure redirectfrom with the shortlink exists
+    if shortlink_url != "":
+        if "redirect_from" in post_frontmatter.keys():
+            redirects_to_this_file = post_frontmatter["redirect_from"]
+            if shortlink_url not in redirects_to_this_file:
+               redirects_to_this_file.append(shortlink_url)
+               save_file = True
+               
+        else:
+           post_frontmatter["redirect_from"] = [ shortlink_url ]
+           save_file = True
+    
+    if save_file:
+        post_file_dump = open(post_full_file_name, "w")
+        post_file_dump.write(frontmatter.dumps(post_frontmatter))
+        post_file_dump.close()
+        files_changed_for_commit += 1
 
 print("====/generate-shortlinks.py================================")
