ad216f63799fa7b26a6152e49118a22c02ed5572
Alon Rotem <alrotem@gmail.com>
2025-01-10 12:53:01 +0200

    Debugging shortlinks script

--- utils/generate-shortlinks.py
+++ utils/generate-shortlinks.py
@@ -22,7 +22,7 @@ if "ALL_CHANGED_FILES" in os.environ.keys():
     print(f"changed_files 1\n{changed_files}")
     print(f"repr(changed_files) 1\n{repr(changed_files)}")
 
-    decoded = changed_files.encode('utf-8').decode('unicode_escape').encode('latin1')
+    decoded = changed_files.replace('\\\\', '\\').encode('utf-8').decode('unicode_escape').encode('latin1')
     print(f"Decoded:\n{decoded}")
 
     filenames = json.loads(changed_files.encode('utf-8').decode('unicode_escape').encode('latin1').decode('utf-8').replace('""','"'))
