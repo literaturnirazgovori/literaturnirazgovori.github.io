78774cb94917d2e42a2d95744721c3da4e3e674a
Alon Rotem <alrotem@gmail.com>
2025-01-10 10:38:06 +0200

    Debugging shortlinks script

--- utils/generate-shortlinks.py
+++ utils/generate-shortlinks.py
@@ -19,7 +19,7 @@ files_changed_for_commit = 0
 
 if "ALL_CHANGED_FILES" in os.environ.keys():
     changed_files = os.environ["ALL_CHANGED_FILES"]
-    print(f"-=-=-\nChanged files:\n{changed_files}\n-=-=-")
+    print(f"-=-=-\nChanged files:\n{changed_files}\nUtf-8 encode:\n{changed_files.encode('utf-8')}-=-=-")
     filenames = json.loads(changed_files.encode('utf-8').decode('unicode_escape'))
 
 # run this script with --allfiles to scan all posts
